b65ba609e8469bc3d94faa62cc8082242108da9b
sunsweet <sunsweet@foxmail.com>
2017-10-15 20:42:31 +0800

    q002

--- 001/q002.cpp
+++ 001/q002.cpp
@@ -3,21 +3,18 @@
 #include <math.h>       /* ceil */
 
 using namespace std;
-
-
 void setOneRow(char * row, char c, int charNum, int rowLength){
 	int blankNum = ceil((float)(rowLength-charNum)/2);
 	int rowCurrentIndex = 0;
-	int i = 0;
+	int i ;
 	if(blankNum > 0)
 	{
-		for(;i<blankNum;i++)
+		for(i=0;i<blankNum;i++)
 		{
 			row[rowCurrentIndex] = ' ';
 			rowCurrentIndex++;
 		}
 	}
-
 	for(i = 0;i<charNum; i++)
 	{
 		row[rowCurrentIndex] = c;
@@ -26,7 +23,7 @@ void setOneRow(char * row, char c, int charNum, int rowLength){
 
 	if(blankNum > 0)
 	{
-		for(;i<blankNum;i++)
+		for(i=0;i<blankNum;i++)
 		{
 			row[rowCurrentIndex] = ' ';
 			rowCurrentIndex++;
@@ -35,11 +32,26 @@ void setOneRow(char * row, char c, int charNum, int rowLength){
 	row[rowCurrentIndex] = '\n';
 	
 }
+
+void displayDiamond(char c,int rowNum){
+	int mid = ceil((float)rowNum/2);
+	int i = 1;
+	int charNum;
+	char row[255];	
+	for(; i <= rowNum; i++){
+		if(i <= mid){
+			charNum = 1+2*(i-1);
+		}else
+        {
+            charNum = rowNum-2*(i-mid);
+        }
+		setOneRow(row,c,charNum,rowNum);
+		printf("%s",row);
+	}
+}
 int main(){
-	char row[255];
 	char c;
 	scanf("%c",&c);
-	setOneRow(row, c, 5,9);
-	printf("%s",row);
+	displayDiamond(c,255);
 	return 0;
 }
